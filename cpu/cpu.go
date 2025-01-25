c8e269c21f2b2d87a979e3dfce825a491ad30c17
Killean Johnson <killean.johnson@viewrail.com>
2025-01-25 12:18:26 -0500

    More refactoring

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -53,11 +53,13 @@ type CPU struct {
 }
 
 func NewCPU() *CPU {
-	return &CPU{
+	cpu := &CPU{
 		Registers:      [RegisterCount]uint8{},
 		Stack:          NewStack(),
 		ProgramCounter: 0,
 	}
+
+	return cpu
 }
 
 func (c *CPU) Execute(memory *Memory) {
@@ -69,206 +71,147 @@ func (c *CPU) Execute(memory *Memory) {
 
 		switch Opcode(opcode) {
 		case OP_LOAD:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			value := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg, value := c.prepRVInstruction(memory)
 			c.Registers[reg] = value
 
 		case OP_STORE_VAL:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			value := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			address, value := c.prepAVInstruction(memory)
 			memory.WriteStoredMemory(uint16(address), value)
 
 		case OP_STORE_REG:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg, address := c.prepRAInstruction(memory)
 			memory.WriteStoredMemory(uint16(address), c.Registers[reg])
 
 		case OP_LOAD_MEM:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg, address := c.prepRAInstruction(memory)
 			c.Registers[reg] = memory.ReadStoredMemory(uint16(address))
 
 		case OP_ADD:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] += c.Registers[reg2]
 
 		case OP_SUB:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] -= c.Registers[reg2]
 
 		case OP_MUL:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] *= c.Registers[reg2]
 
 		case OP_DIV:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] /= c.Registers[reg2]
 
 		case OP_MOD:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] %= c.Registers[reg2]
 
 		case OP_AND:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] &= c.Registers[reg2]
 
 		case OP_OR:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] |= c.Registers[reg2]
 
 		case OP_XOR:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Registers[reg1] ^= c.Registers[reg2]
 
 		case OP_NOT:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.Registers[reg] = ^c.Registers[reg]
 
 		case OP_SHL:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.Registers[reg] <<= 1
 
 		case OP_SHR:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.Registers[reg] >>= 1
 
 		case OP_INC:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.Registers[reg]++
 
 		case OP_DEC:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.Registers[reg]--
 
 		case OP_JMP:
-			address := memory.Read(c.ProgramCounter)
+			address := c.prepAInstruction(memory)
 			c.ProgramCounter = uint16(address)
 
 		case OP_JMP_REG:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.ProgramCounter = uint16(c.Registers[reg])
 
 		case OP_PUSH:
-			value := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			value := c.prepVInstruction(memory)
 			c.Stack.Push(value)
 
 		case OP_PUSH_REG:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.Stack.Push(c.Registers[reg])
 
 		case OP_POP:
+			c.prepNoneInstruction()
 			c.Stack.Pop()
-			c.ProgramCounter++
 
 		case OP_POP_REG:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			c.Registers[reg] = c.Stack.Pop()
 
 		case OP_CMP_REG_VAL:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			value := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg, value := c.prepRVInstruction(memory)
 			c.Flags.Compare(c.Registers[reg], value)
 
 		case OP_CMP_REG_REG:
-			reg1 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
-			reg2 := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg1, reg2 := c.prepRRInstruction(memory)
 			c.Flags.Compare(c.Registers[reg1], c.Registers[reg2])
 
 		case OP_JE:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			address := c.prepAInstruction(memory)
 			if c.Flags.Equal == 1 {
 				c.ProgramCounter = uint16(address)
 			}
 
 		case OP_JNE:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			address := c.prepAInstruction(memory)
 			if c.Flags.Equal == 0 {
 				c.ProgramCounter = uint16(address)
 			}
 
 		case OP_JG:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			address := c.prepAInstruction(memory)
 			if c.Flags.Greater == 1 {
 				c.ProgramCounter = uint16(address)
 			}
 
 		case OP_JGE:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			address := c.prepAInstruction(memory)
 			if c.Flags.Greater == 1 || c.Flags.Equal == 1 {
 				c.ProgramCounter = uint16(address)
 			}
 
 		case OP_JL:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			address := c.prepAInstruction(memory)
 			if c.Flags.Less == 1 {
 				c.ProgramCounter = uint16(address)
 			}
 
 		case OP_JLE:
-			address := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			address := c.prepAInstruction(memory)
 			if c.Flags.Less == 1 || c.Flags.Equal == 1 {
 				c.ProgramCounter = uint16(address)
 			}
 
 		case OP_PRINT:
-			value := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			value := c.prepVInstruction(memory)
 			fmt.Println(value)
 
 		case OP_PRINT_REG:
-			reg := memory.Read(c.ProgramCounter)
-			c.ProgramCounter++
+			reg := c.prepRInstruction(memory)
 			fmt.Println(c.Registers[reg])
 
 		case OP_HLT:
@@ -278,4 +221,58 @@ func (c *CPU) Execute(memory *Memory) {
 			panic("Unknown opcode")
 		}
 	}
+}
+
+func (c *CPU) prepRRInstruction(memory *Memory) (reg1, reg2 uint8) {
+	reg1 = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	reg2 = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	return reg1, reg2
+}
+
+func (c *CPU) prepRVInstruction(memory *Memory) (reg uint8, value uint8) {
+	reg = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	value = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	return reg, value
+}
+
+func (c *CPU) prepRAInstruction(memory *Memory) (reg, address uint8) {
+	reg = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	address = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	return reg, address
+}
+
+func (c *CPU) prepRInstruction(memory *Memory) (reg uint8) {
+	reg = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	return reg
+}
+
+func (c *CPU) prepAVInstruction(memory *Memory) (address, value uint8) {
+	address = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	value = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	return address, value
+}
+
+func (c *CPU) prepAInstruction(memory *Memory) (address uint8) {
+	address = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	return address
+}
+
+func (c *CPU) prepVInstruction(memory *Memory) (value uint8) {
+	value = memory.Read(c.ProgramCounter)
+	c.ProgramCounter++
+	return value
+}
+
+func (c *CPU) prepNoneInstruction() {
+	c.ProgramCounter++
 }
